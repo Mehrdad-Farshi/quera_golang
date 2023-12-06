e442b86c4ac8026d7d3df852fba0ffe3afd82c0a
Mehrdad FarshiAhmadi <mehrdad.arjavand@gmail.com>
2023-12-06 17:50:29 +0330

    fallthrough test

--- chapter1/variable/main.go
+++ chapter1/variable/main.go
@@ -3,17 +3,14 @@ package main
 import "fmt"
 
 func main() {
-name:="ali"
-switch name {
-case "ahmad":
-    fmt.Println("Hello ahmad")
-case "mohammad":
-    fmt.Println("Hello mohammad")
-case "ali":
-    fmt.Println("Hello ali")
-case "hossein":
-    fmt.Println("Hello hossein")
-default:
-    fmt.Println("Hello")
-}
+name := "ahmad"
+    switch name {
+        case "ahmad":
+        fmt.Println("Hello Ahmad")
+        fallthrough
+        case "mohammad":
+        fmt.Println("Greetings")
+        default:
+        fmt.Println("How are you?")
+    }
 }
