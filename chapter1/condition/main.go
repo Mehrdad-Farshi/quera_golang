c123896cca01aacf9bc1231ce30a2efbd5e3a56c
Mehrdad FarshiAhmadi <mehrdad.arjavand@gmail.com>
2023-12-13 12:21:00 +0330

    switch case added to directory conditions

---
+++ chapter1/condition/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+<<<<<<< HEAD
+name := "ahmad"
+    switch name {
+        case "ahmad":
+        fmt.Println("Hello Ahmad")
+        fallthrough
+        case "mohammad":
+        fmt.Println("Greetings")
+        default:
+        fmt.Println("How are you?")
+    }
+}
