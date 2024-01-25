33271a96ea4de3a16d0ef13b33e4a9b25479d184
Mehrdad-Farshi <meh1376@hotmail.com>
2024-01-25 08:18:14 +0330

    initial commit for hope game

--- chapter1/hope/main.go
+++ chapter1/hope/main.go
@@ -1,19 +1,16 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 )
 
 func main() {
-	var number int
-
-	fmt.Print("Enter an integer: ")
-	_, err := fmt.Scan(&number)
-	fmt.Println("number:", number)
-	if err != nil {
-		fmt.Println("Error reading input:", err)
-		return
+	var q, p int
+	fmt.Println("Type two numbers: q ( end of game) and then p (mazrab) ")
+	if q == 0 {
+		return "error", errors.New("negetive number")
 	}
-
-	fmt.Println("number:", number)
+	fmt.Scanln(&q, &p)
+	fmt.Println("q is equal to = ", q, ", p is equal to = ", p)
 }
