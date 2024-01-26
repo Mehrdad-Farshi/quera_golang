bb7d7f1190e052dd1b4f2da1593c062bab6e62db
Mehrdad-Farshi <meh1376@hotmail.com>
2024-01-26 19:39:59 +0330

    defined a 2 dimantions array

--- chapter1/variables/main.go
+++ chapter1/variables/main.go
@@ -2,35 +2,15 @@ package main
 
 import "fmt"
 
-var (
-	name      string = "mehrdad"
-	age       int    = 26
-	isStudent bool   = true
-)
-
-var (
-	income  float64
-	taxRate float64
-)
-
-const (
-	floor1 int = 111
-)
-
-var (
-	seasons [4]string
-)
-
 func main() {
-	seasons[0] = "spring"
-	seasons[1] = "summer"
-	seasons[2] = "fall"
-	seasons[3] = "winter"
-	for i := 0; i < len(seasons); i++ {
-		fmt.Printf("at index %v Elemnet  = %v  \n", i, seasons[i])
-	}
-	fmt.Printf("-------------------------------------------------\n")
-	for range seasons {
-		fmt.Printf("iterated \n")
+	var tic_tac_toe = [4][3]bool{{true, true, false},
+		{false, false, false},
+		{true, false, true},
+		{true, false, true}}
+
+	// fmt.Println(tic_tac_toe)
+
+	for index, element := range tic_tac_toe {
+		fmt.Printf("Element %v At Index %d\n", element, index)
 	}
 }
