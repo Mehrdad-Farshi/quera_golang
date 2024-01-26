b7abeb456ef431144b02fc9c758c616fda3f9ba6
Mehrdad-Farshi <meh1376@hotmail.com>
2024-01-26 19:54:16 +0330

    slice

--- chapter1/variables/main.go
+++ chapter1/variables/main.go
@@ -3,14 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	var tic_tac_toe = [4][3]bool{{true, true, false},
-		{false, false, false},
-		{true, false, true},
-		{true, false, true}}
+	var smehrdad []string
 
-	// fmt.Println(tic_tac_toe)
-
-	for index, element := range tic_tac_toe {
-		fmt.Printf("Element %v At Index %d\n", element, index)
-	}
+	fmt.Println(smehrdad)
+	fmt.Println(smehrdad == nil)
 }
